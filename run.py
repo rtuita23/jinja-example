2d0aebd107385f19baeb1a9ee80e0f41036bfe03
rtuita23 <rktuita23@hotmail.com>
2015-02-18 02:24:05 +0000

    v3

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
-from datetime import datetime
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def template_test():
-    return render_template('template.html', my_string='Wheeeee!', my_list=[0,1,2,3,4,5])
+    return render_template('template.html', my_string='Wheeeee!', my_list=[0,1,2,3,4,5],     current_time=datetime.datetime.now())
 
 @app.route("/home")
 def home():
     return render_template('template.html',
                            my_string="I'm the home page",
                            my_list=[0,1,2,3,4,5],
-                          
+                           current_time=datetime.datetime.now()
                           )
 
 @app.route("/about")
@@ -20,7 +20,7 @@ def about():
     return render_template('template.html',
                             my_string="I'm the about page",
                             my_list=[0,1,2,3,4,5],
-                            
+                            current_time=datetime.datetime.now()
                           )
 
 @app.route("/contact")
@@ -28,8 +28,12 @@ def contact():
     return render_template('template.html',
                            my_string="I'm the contact page",
                            my_list=[0,1,2,3,4,5],
-                           
+                           current_time=datetime.datetime.now()
                           )
+@app.template_filter()
+def datetimefilter(value, format='%Y/%m/%d %H:%M'):
+    """Convert a datetime to a different format."""
+    return value.strftime(format)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
